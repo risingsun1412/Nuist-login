a233d55cba54a1680dc6eb2a1d8f3117b17d0768
zhou <44549736+risingsun1412@users.noreply.github.com>
2019-11-13 20:28:29 +0800

    Add files via upload

--- login.py
+++ login.py
@@ -10,24 +10,24 @@ def main(username, domain, password, times):
         'password': password,
         'enablemacauth': '0',
     }
-    print('正在发送登陆请求(第%d次)......'%times)
-    try:
-        response = post(url, data = pyload)
-        if response.status_code == 200:
-            print('请求发送成功！！')
-        else:
-            print('请求发送失败，请检查网络状态！！！')
-        test = get('https://www.baidu.com')
-        if test.status_code == 200:
-            print('登陆成功！！')
-            return
-        else:
-            print(test.status_code)
-    except:
-        print('登陆失败！！！')
-        sleep(0.2)
-        print('尝试重新登陆!')
+    print('[*]正在发送登陆请求(第%d次)......'%times)
+    
+    response = post(url, data = pyload)
+    if response.status_code == 200:
+        print('[*]请求发送成功！！')
+    else:
+        print('[!]请求发送失败，请检查网络状态！！！')
+    test = get('http://baidu.com')
+    if (test.text.find("南京信息工程大学信息化建设与管理处") == -1):
+        print('[*]登陆成功!')
+        return
+    else:
+        print('[!]网络认证失败!')
+        sleep(1)
+        print('[*]尝试重新登陆!')
         main(username, domain, password, times + 1)
+        
+        
     
 if __name__ == "__main__":
     main('', 'ChinaNet', '', 1)# 账号，运营商，密码(base64)
